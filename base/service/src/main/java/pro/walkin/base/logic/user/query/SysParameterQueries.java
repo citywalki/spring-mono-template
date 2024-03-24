f239547f9b2ac3ac106f26300d2ccf47eccf941a
walkin <wyc19966@hotmail.com>
2024-03-24 16:52:20 +0800

    init

--- base/service/src/main/java/pro/walkin/base/logic/user/query/SysParameterQueries.java
+++ base/service/src/main/java/pro/walkin/base/logic/user/query/SysParameterQueries.java
@@ -1,8 +1,10 @@
 package pro.walkin.base.logic.user.query;
 
+import org.springframework.stereotype.Service;
 import pro.walkin.base.domain.parameter.SysParameter;
 import pro.walkin.base.logic.user.repository.SysParameterRepository;
 import pro.walkin.framework.query.AbstractQueries;
 
+@Service
 public class SysParameterQueries extends AbstractQueries<SysParameter, SysParameterRepository> {
 }
